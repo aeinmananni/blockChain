491bdf351ff1cd967965787405a6113675373004
aein <mananniaein1234@gmail.com>
2025-10-05 04:40:31 +0330

    update new Chnage

--- scripts/blockChain/index.ts
+++ scripts/blockChain/index.ts
@@ -1,7 +1,7 @@
-import { GENERATE_NEW_BLOCK_HANDLER } from "../../utils/generate-new-block.util";
-import { GENESIS } from "../../utils/genesis.util";
-import { BlockType } from "../models/script.model";
-
+import { GENERATE_NEW_BLOCK_HANDLER } from '../../utils/generate-new-block.util';
+import { GENESIS } from '../../utils/genesis.util';
+import { BlockType } from '../models/script.model';
+import { CRYPTO_HASH_HANDLER } from '../../utils/crypto-hash';
 export const BlockChain = () => {
   let chain: BlockType[] = [GENESIS];
 
@@ -10,9 +10,22 @@ export const BlockChain = () => {
     chain = [...chain, NEW_BLOCK];
   };
 
+  const isValidChain = (chain: BlockType[]) => {
+    if (chain[0] !== GENESIS) return false;
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i];
+      const actualLastHash = chain[i].hash;
+      const { hash, lastHash, timestamp, data } = block;
+
+      if (lastHash !== actualLastHash) return false;
+      if (hash !== CRYPTO_HASH_HANDLER(lastHash, data, timestamp)) return false;
+    }
+
+    return true;
+  };
   const getChain = (): BlockType[] => {
     return chain;
   };
 
-  return { addBlock, getChain };
+  return { addBlock, getChain, isValidChain };
 };
